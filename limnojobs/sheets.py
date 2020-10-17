8320d9a90f97f1e54c3ce712b7a61087f5e0cb59
jsta <stachel2@msu.edu>
2020-10-17 12:20:37 -0400

    working filter for sheets data

--- limnojobs/sheets.py
+++ limnojobs/sheets.py
@@ -7,7 +7,6 @@ gc = gspread.service_account()
 # pull_ecoevo
 data_start_row = 3
 sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1hf_q-3gdyOlsk97I3OW97w_cmQXsKQVC-ZGDMgFnL2I/edit#gid=1954069648")
-worksheet = sh.get_worksheet(0)
 
 dt_raw = sh.values_batch_get([
     'Faculty Jobs!G3:G',
@@ -22,28 +21,15 @@ dt = pd.DataFrame({
     "institution": dt_raw['valueRanges'][2]['values'], 
     "notes": dt_raw['valueRanges'][3]['values']    
 })
-subject = dt['rank'] + dt['subject'] + dt['institution'] 
+dt = dt.applymap(lambda x:  ''.join(map(str, x)))
+
+subject = dt['rank'] + ' | ' + dt['subject'] + ' | ' + dt['institution'] 
 body = dt['notes']
 
 res = pd.DataFrame({
     "subject": subject,
     "body": body
 })
-
-test = []
-for a in res['subject'][0]:
-    test.append(a)
-
-res['subject'][0]
-
-flatten = lambda l: [item for sublist in l for item in sublist]
-
-s=res.apply(lambda x : x.str.len())<3
-df.loc[:,column_list]=df.loc[:,column_list].mask(s)
-
-print(res.body.replace(r'\[\]', np.nan, regex=True))
-
-res.body.replace("[]", 1)
 
 test = utils.filter_limno(res)
 
