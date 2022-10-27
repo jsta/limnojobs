e40284b509d2387cdb5d11c4dd624b02ceee51e8
jsta <stachel2@msu.edu>
2022-10-27 14:01:02 -0600

    formatting with black

--- limnojobs/limnojobs.py
+++ limnojobs/limnojobs.py
@@ -14,21 +14,21 @@ import sheets
 import rss
 import boards
 
-currentdir = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-def limnotoots(tweet = False, interactive = False, unseen = True):
+
+def limnotoots(tweet=False, interactive=False, unseen=True):
     r"""Filter limnology themed papers from a pandas DataFrame.
     :param tweet: boolean. Post tweets of limnojobs
-    :param interactive: boolean. Ask for approval before tweeting.    
+    :param interactive: boolean. Ask for approval before tweeting.
     :param unseen: boolean. Limit listserv queries to only unseen messages
     """
 
     def print_source(x):
-        sys.stdout.write('\b' * 1000)
-        sys.stdout.write('Fetching jobs from: ' + x + '\r')
+        sys.stdout.write("\b" * 1000)
+        sys.stdout.write("Fetching jobs from: " + x + "\r")
 
     print_source("RSS")
     jobs_rss = rss.pull_rss()
@@ -38,7 +38,7 @@ def limnotoots(tweet = False, interactive = False, unseen = True):
     jobs_rse = boards.pull_rse()
     print_source("PANGEO")
     jobs_pangeo = boards.pull_pangeo()
-    # print_source("ECOLOG")    
+    # print_source("ECOLOG")
     # jobs_ecolog = listservs.pull_ecolog(unseen = unseen)
     print_source("ECOEVO")
     jobs_ecoevo = sheets.pull_ecoevo()
@@ -47,16 +47,18 @@ def limnotoots(tweet = False, interactive = False, unseen = True):
     # jobs_boards = pull_boards()
 
     ## append/concat data sources
-    jobs_all = pd.concat([jobs_ecoevo, jobs_earthenvscience, jobs_rss, jobs_csdms, jobs_rse, jobs_pangeo])
+    jobs_all = pd.concat(
+        [jobs_ecoevo, jobs_earthenvscience, jobs_rss, jobs_csdms, jobs_rse, jobs_pangeo]
+    )
     # jobs_all = jobs_ecolog
 
     ## test against log
     log = pd.read_csv("log.csv")
-    res = jobs_all[~jobs_all['subject'].isin(log['subject'])]# [['subject']]
-    res = res.sample(frac = 1)  # randomize jobs order
+    res = jobs_all[~jobs_all["subject"].isin(log["subject"])]  # [['subject']]
+    res = res.sample(frac=1)  # randomize jobs order
 
     ## print/offer tweets
-    jobs = res['subject']
+    jobs = res["subject"]
     print(Fore.GREEN + "Filtered: ")
     print()
     for job in jobs:
@@ -64,65 +66,63 @@ def limnotoots(tweet = False, interactive = False, unseen = True):
         print(Fore.GREEN + job)
         print()
 
-    if(tweet is True or interactive is True):
-        api = twitter.Api(consumer_key=config.api_key,
-                        consumer_secret=config.api_secret_key,
-                        access_token_key = config.access_token_key,
-                        access_token_secret=config.access_token_secret)
+    if tweet is True or interactive is True:
+        api = twitter.Api(
+            consumer_key=config.api_key,
+            consumer_secret=config.api_secret_key,
+            access_token_key=config.access_token_key,
+            access_token_secret=config.access_token_secret,
+        )
         # api.VerifyCredentials()
 
         for i in range(len(res)):
-            # i = 0            
+            # i = 0
             job = jobs.iloc[i]
             print(job)
-            if(interactive is True):
+            if interactive is True:
                 post_job = input("post limnojob (y)/n/i? ") or "y"
-                if(post_job in ["y"]):
+                if post_job in ["y"]:
                     status = api.PostUpdate(job)
                     posted = "y"
-                if(post_job in ["i"]):
+                if post_job in ["i"]:
                     posted = "i"
 
-                if(post_job in ["y", "i"]):
-                    # write to log                
+                if post_job in ["y", "i"]:
+                    # write to log
                     keys = ["source", "date", "flag", "subject", "body"]
-                    source = res.iloc[i]['source']
+                    source = res.iloc[i]["source"]
                     date = str(datetime.date.today())
-                    subject = res.iloc[i]['subject']
-                    body = res.iloc[i]['body']                
-                    d = dict(zip(keys, [source, date, posted,
-                                        subject, body]))
+                    subject = res.iloc[i]["subject"]
+                    body = res.iloc[i]["body"]
+                    d = dict(zip(keys, [source, date, posted, subject, body]))
 
                     d = pd.DataFrame.from_records(d, index=[0])
-                    log = log.append(pd.DataFrame(data = d), ignore_index = True)
-                    log.to_csv("log.csv", index = False)
+                    log = log.append(pd.DataFrame(data=d), ignore_index=True)
+                    log.to_csv("log.csv", index=False)
             else:
                 # status = api.PostUpdate(toot)
 
                 keys = ["source", "date", "flag", "subject", "body"]
-                source = res.iloc[i]['source']
+                source = res.iloc[i]["source"]
                 date = str(datetime.date.today())
-                subject = res.iloc[i]['subject']
-                body = res.iloc[i]['body']                
-                d = dict(zip(keys, [source, date, posted,
-                                    subject, body]))
+                subject = res.iloc[i]["subject"]
+                body = res.iloc[i]["body"]
+                d = dict(zip(keys, [source, date, posted, subject, body]))
 
                 d = pd.DataFrame.from_records(d, index=[0])
-                log = log.append(pd.DataFrame(data = d), ignore_index = True)
-                log.to_csv("log.csv", index = False)
+                log = log.append(pd.DataFrame(data=d), ignore_index=True)
+                log.to_csv("log.csv", index=False)
             post_job = "n"
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tweet', default = False,
-                        action='store_true')
-    parser.add_argument('--interactive', default = False,
-                        action='store_true')
-    parser.add_argument('--unseen', default = True,
-                        action='store_false')
+    parser.add_argument("--tweet", default=False, action="store_true")
+    parser.add_argument("--interactive", default=False, action="store_true")
+    parser.add_argument("--unseen", default=True, action="store_false")
     args = parser.parse_args()
 
-    limnotoots(tweet = args.tweet, interactive = args.interactive, unseen = args.unseen)
+    limnotoots(tweet=args.tweet, interactive=args.interactive, unseen=args.unseen)
 
 
 if __name__ == "__main__":
